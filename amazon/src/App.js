cadbbc9a439af02c136b21746697640bee60983d
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-05-04 02:59:06 +0530

    setup

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Routes,Route,BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/" >
+      <Header/>
+      <Home/>
+      </Route>
+
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
