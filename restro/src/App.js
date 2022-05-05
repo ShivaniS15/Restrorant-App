241814ca5c6e00a2fa00d76a6a92f7f5b28993a7
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-05-05 22:21:15 +0530

    header

--- restro/src/App.js
+++ restro/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 export const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
+    <div className="w-screen h-screen flex ">
     <Header/>
     </div>
   )
