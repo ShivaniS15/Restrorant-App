8c75907ab85bc00f008b344d3dddb14cbe067a84
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-05-05 23:04:32 +0530

    header-done

--- restro/src/App.js
+++ restro/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Header from './components/Header'
-
+import {Header,CreateContainer,MainContainer} from './components'
+import {Routes,Route} from "react-router-dom";
 export const App = () => {
   return (
-    <div className="w-screen h-screen flex ">
-    <Header/>
-    </div>
+    <div className="w-screen h-auto flex flex-col bg-primary">
+  <Header />
+  <main className="mt-24 p-8 w-full">
+  <Routes>
+     <Route path="/*" element={<MainContainer />} />
+     <Route path="/createItem" element={<CreateContainer />} />
+</Routes>
+  </main>
+</div>
   )
 }
--- restro/src/components/Header.jsx
+++ restro/src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import Logo from "./img/logo.png"
+import Logo from "../img/logo.png"
+import { Link } from 'react-router-dom';
+import Avatar from "../img/avatar.png"
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { MdShoppingBasket } from "react-icons/md";
 const Header = () => {
   return (
+      <AnimatePresence>
     <header className="fixed z-50 w-screen p-6 px-16">
  {/* desktop&tablet */}
- <div className="hidden md:flex w-full h-full">
-  <div className="flex items-center gap-2">
+ <div className="hidden md:flex w-full h-full items-center justify-between">
+  <Link to={"/"} className="flex items-center gap-2">
       <img src={Logo} className="w-8 object-cover" alt="logo" />
    <p className="text-headingColor text-xl font-bold"> City</p>
- </div>
- <ul className="flex items-center gap-8 ml-auto">
+ </Link>
+ <div className='flex items-center'>
+ <ul className="flex items-center gap-8 ">
  <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Home
             </li>
@@ -26,16 +32,25 @@ const Header = () => {
  </ul>
  <div className="relative flex items-center justify-center">
   <MdShoppingBasket className=" text-textColor text-2xl ml-8 cursor-pointer" />
-   <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex
+   <div className=" absolute -top-3 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex
  items-center justify-center">
    <p className="text-xs  text-white font-semibold">2</p>
  </div>
 </div>
-                                      
-</div>
+{/* Avatar */}
+<motion.img
+whileTap={{ scale: 0.6 }}
+ src={Avatar}
+ className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl ml-6 -top-2"
+ alt="userprofile"
+ />
+
+ </div>
+ </div>
  {/* mobile */}
   <div className="flex md:hidden w-full h-full "></div>
 </header>
+</AnimatePresence>
   )
 }
 
